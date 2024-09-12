da4b1ef2240189d5dce42e7f8bf44012d26beed8
mmaalin <maalin.622139@swl.k12.oh.us>
2024-09-12 08:54:31 -0400

    lab 04 extra credit evenodd

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -1,5 +1,19 @@
-public class Main {
+import java.util.Scanner;
+
+public class EvenOdd {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello! Please input an integer of your choice.");
+
+        Scanner scan = new Scanner (System.in);
+        int userNum = scan.nextInt();
+
+        if (userNum % 2 == 0) {
+            System.out.print("The number " + userNum + " is even.");
+        }
+            else {
+                System.out.println("The number " + userNum + " is odd.");
+            }
+        }
+
     }
-}
